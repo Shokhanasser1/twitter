51c59c3526b69e850c617532ad4030f9fc5fcaea
Shokhanasser <shokhanasserprojr@gmail.com>
2024-06-21 21:10:34 +0500

    third video work

--- base/models.py
+++ base/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -11,3 +12,15 @@ class Profile(models.Model):
     
     def __str__(self) -> str:
         return self.user.username
+
+# Create Profile When New User Signs Up
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+        #Have the user follow themselves
+        user_profile.follows.set([instance.profile.id])
+        user_profile.save()
+        
+post_save.connect(create_profile, sender=User)
+
