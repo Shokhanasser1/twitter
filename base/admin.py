b147732a856262f8accb7dda2dcecd5c719215aa
Shokhanasser <shokhanasserprojr@gmail.com>
2024-06-21 19:31:20 +0500

    second video work

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
+from .models import Profile
+# Unregister groups
+admin.site.unregister(Group)
 
-# Register your models here.
+# Mix profile info into user info
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    
+# Extend User Model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    #Just deisplay username fiels on admin page
+    fields = ['username']
+    inlines = [ProfileInline]
+    
+# Unregister initial User
+admin.site.unregister(User)
+# Register User
+admin.site.register(User, UserAdmin)
+# admin.site.register(Profile)
+
